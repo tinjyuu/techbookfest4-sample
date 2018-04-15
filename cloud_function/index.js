3e64f9085105d1b0a4a24168dc6905cecdb4eaa5
shouta.yoshikai <shouta.yoshikai@kabuku.co.jp>
2018-04-15 17:02:59 +0900

    update index.js

--- cloud_function/index.js
+++ cloud_function/index.js
@@ -5,12 +5,9 @@ const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
  
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
-
 // init firebase
 const admin = require('firebase-admin');
-
 admin.initializeApp(functions.config().firebase);
-
 var db = admin.firestore();
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
@@ -20,14 +17,12 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   const parameters = request.body.result.parameters;
 
   function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
-    
-
+    agent.add(`ようこそ`);
   }
  
   function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
+    agent.add(`ヘルシェイク矢野ことを考えていて聞いていませんでした`);
+    agent.add(`もう一度お願いします`);
 }
 
 function start(agent) {
@@ -56,34 +51,6 @@ function command(agent) {
     });
     agent.add(`${parameters.command} を実行します`);
 }
-
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://assistant.google.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
-
-  // // Uncomment and edit to make your own Google Assistant intent handler
-  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function googleAssistantHandler(agent) {
-  //   let conv = agent.conv(); // Get Actions on Google library conv instance
-  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-  // }
-
 // Run the proper function handler based on the matched Dialogflow intent name
 let intentMap = new Map();
 intentMap.set('Default Welcome Intent', welcome);
@@ -91,7 +58,5 @@ intentMap.set('Default Fallback Intent', fallback);
 intentMap.set('command', command);
 intentMap.set('start', start);
 intentMap.set('end', end);
-// intentMap.set('your intent name here', yourFunctionHandler);
-// intentMap.set('your intent name here', googleAssistantHandler);
 agent.handleRequest(intentMap);
 });
